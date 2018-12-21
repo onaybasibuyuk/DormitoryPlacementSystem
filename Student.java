5dc592ab7174e77143dc1175802df1f46c61deac
onaybasibuyuk <43809941+onaybasibuyuk@users.noreply.github.com>
2018-12-21 22:58:49 +0300

    Update Student.java

--- Student.java
+++ Student.java
@@ -31,6 +31,10 @@ public class Student {
 		return age;
 	}
 	
+	public void setHomeCity(String homeCity){
+		this.homeCity = homeCity;
+	}
+	
 	public String getHomeCity() {
 		return homeCity;
 	}
